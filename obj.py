f3929930ceb9209173c7eb3da5feafb331856029
AlOkhrimenko <91527442+alokhrimenko@users.noreply.github.com>
2021-12-10 16:16:25 +0300

    Updated obj.py for testing

--- obj.py
+++ obj.py
@@ -2,11 +2,11 @@ import vector
 
 
 class Sphere:
-    def __init__(self, pos: VectorObject3D, r, scale=1, del=False):
+    def __init__(self, pos: vector.VectorObject3D, r, scale=1, delete=False):
         self.pos = pos
         self.r = r
         self.scale = scale
-        self.del = del
+        self.delete = delete
 
-    def dist(self, point: VectorObjeect3D):
-        return scale * abs(self.pos - point)
+    def dist(self, point: vector.VectorObject3D):
+        return self.scale * (abs(self.pos - point) - self.r)
