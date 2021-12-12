fa1f9533aa62a6109e78f5983d18afef86de87b7
apBUSampK <apbusampk@gmail.com>
2021-12-13 01:05:58 +0300

    Updated camera.py: rejected active camera idea: CPU rendering is too slow. Moved all tkinter-related code away from camera.py module: preparing to switch tkinter for PIL. Corrected ray direction selection during rendering. Implemented multi-threading (enabled by '-m' flag). Created basic scene for debugging. Added fancy progress bar (spinner for multithreading).

--- camera.py
+++ camera.py
@@ -1,99 +1,71 @@
-import tkinter
+from copy import deepcopy
+from progress.bar import ChargingBar
+from progress.spinner import Spinner
+from multiprocessing import Pool
 
-import numpy
 import vector
+from numpy import pi, tan
 
 ROT_SPEED = .1
 MOV_SPEED = 10
-ITERATIONS = 50
-MAX_DIST = 1000
+ITERATIONS = 200
+MAX_DIST = 200
 Ox = vector.obj(x=1, y=0, z=0)
 Oy = vector.obj(x=0, y=1, z=0)
 Oz = vector.obj(x=0, y=0, z=1)
 
 
-class Camera(tkinter.Canvas):
+class Camera():
 
     """
-    Класс камеры. Наследует виджет-класс Canvas модуля Tkinter. Метод update() перегружен для отрисовки сцены.
+    Класс камеры.
     Метод renderer() производит попиксельную отрисовку рейкастом с алгоритмом реймаршинга
-    Перемещение камеры по сцене - WASD + LShift и LAlt для вертикального смещения
-    Поворот камеры - стрелки клавиатуры (крен не предусмотрен)
     """
 
-    def __init__(self, master: tkinter.Tk, catalogue, width, height, fov_h=90, position=vector.obj(x=0, y=0, z=0)):
-        super().__init__(master, height=height, width=width, bg="black")
+    def __init__(self, width, height, catalogue, fov=90, position=vector.obj(x=0, y=0, z=0)):
         self.height = height
         self.width = width
-        self.fov_h = fov_h / 180 * numpy.pi
-        self.fov_v = fov_h / width * height
+        fov_h = fov / 360 * pi
+        self.scale_x = tan(fov_h)
+        self.scale_y = self.scale_x / width * height
         self.pos = position
         self.dir = Ox
         self.left = Oy
         self.up = Oz
         self.catalogue = catalogue
-        self.theta = self.phi = 0
-        self.forward = self.sideways = self.upwards = 0
-        self.bind('<KeyPress-Left>', self._chphi(-ROT_SPEED))
-        self.bind('<KeyRelease-Left>', self._chphi(ROT_SPEED))
-        self.bind('<KeyPress-Right>', self._chphi(ROT_SPEED))
-        self.bind('<KeyRelease-Right>', self._chphi(-ROT_SPEED))
-        self.bind('<KeyPress-Up>', self._chtheta(-ROT_SPEED))
-        self.bind('<KeyRelease-Up>', self._chtheta(ROT_SPEED))
-        self.bind('<KeyPress-Down>', self._chtheta(ROT_SPEED))
-        self.bind('<KeyRelease-Down>', self._chtheta(-ROT_SPEED))
-        self.bind('<KeyPress-w>', self._chfwd(1))
-        self.bind('<KeyRelease-w>', self._chfwd(-1))
-        self.bind('<KeyPress-s>', self._chfwd(-1))
-        self.bind('<KeyRelease-s>', self._chfwd(1))
-        self.bind('<KeyPress-a>', self._chsdw(1))
-        self.bind('<KeyRelease-a>', self._chsdw(-1))
-        self.bind('<KeyPress-d>', self._chsdw(-1))
-        self.bind('<KeyRelease-d>', self._chsdw(1))
-        self.bind('<KeyPress-Shift_L>', self._chup(1))
-        self.bind('<KeyRelease-Shift_L>', self._chup(-1))
-        self.bind('<KeyPress-Alt_L>', self._chup(-1))
-        self.bind('<KeyRelease-Alt_L>', self._chup(1))
 
-    def _chtheta(self, val):
-        self.theta += val
+    def render_pixel(self, i, j, spinner=None):
+        ray_pos = deepcopy(self.pos)
+        ray_dir = self.dir + self.left * (self.width - 2*i) / self.width * self.scale_x +\
+            self.up * (self.height - 2*j) / self.height * self.scale_y
+        ray_dir /= abs(ray_dir)
+        for iter in range(ITERATIONS):
+            if abs(ray_pos - self.pos) > MAX_DIST:
+                if spinner is not None:
+                    spinner.next()
+                return i, j, False
+            dist = min([i.dist(ray_pos) for i in self.catalogue])
+            if dist <= 0:
+                break
+            else:
+                ray_pos += ray_dir * dist
+        if spinner is not None:
+            spinner.next()
+        return i, j, True
 
-    def _chphi(self, val):
-        self.phi += val
+    def mp_render(self):
+        with Pool() as p:
+            spinner = Spinner("Rendering")
+            args = [(i, j, spinner) for i in range(self.width) for j in range(self.height)]
+            render_map = p.starmap(self.render_pixel, args)
+            spinner.finish()
+            return render_map
 
-    def _chfwd(self, val):
-        self.forward += val
-
-    def _chsdw(self, val):
-        self.sideways += val
-
-    def _chup(self, val):
-        self.upwards += val
-
-    def renderer(self):
+    def render(self):
+        bar = ChargingBar('Rendering', max=self.width * self.height)
+        render_map = []
         for i in range(self.width):
             for j in range(self.height):
-                ray_pos = self.pos
-                ray_dir = self.dir.rotate_axis(self.up, self.fov_h * (self.width/2 - i/self.width)).\
-                    rotate_axis(self.left, self.fov_v * (self.height/2 - i/self.width))
-                for iter in range(ITERATIONS):
-                    if abs(ray_pos - self.pos) > MAX_DIST:
-                        break
-                    dist = min([i.dist(ray_pos) for i in self.catalogue])
-                    if dist <= 0:
-                        bright = int(255 * iter / ITERATIONS)
-                        self.create_rectangle(i, j, i, j, outline="#{:x}{:x}{:x}".format(bright, bright, bright))
-                        break
-                    else:
-                        ray_pos += ray_dir * dist
-
-    def update(self):
-        speed = self.dir * self.forward + self.left * self.sideways + self.up * self.upwards
-        speed = speed * MOV_SPEED / abs(speed)
-        self.pos += speed
-        self.dir.rotate_axis(self.up, self.phi)
-        self.left.rotate_axis(self.up, self.phi)
-        self.dir.rotate_axis(self.left, self.theta)
-        self.up.rotate_axis(self.left, self.theta)
-        self.renderer()
-        super().update()
+                render_map.append(self.render_pixel(i, j))
+                bar.next()
+        bar.finish()
--- main.py
+++ main.py
@@ -1,13 +1,33 @@
+import sys
 import tkinter
+import vector
 import camera
+import obj
 
 WINDOW_WIDTH = 400
 WINDOW_HEIGHT = 300
 
-if __name__ == '__main__':
-    objects = []
+
+def main(argv):
+    objects = [obj.Sphere(vector.obj(x=50, y=0, z=0), 10)]
     window = tkinter.Tk()
-    cam = camera.Camera(window, objects, WINDOW_WIDTH, WINDOW_HEIGHT)
-    cam.pack()
-    while True:
-        cam.update()
+    cam = camera.Camera(WINDOW_WIDTH, WINDOW_HEIGHT, objects)
+    canv = tkinter.Canvas(window, background='black', width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
+    canv.pack()
+    if argv is None:
+        render_map = cam.render()
+    else:
+        if argv[0] == '-m':
+            render_map = cam.mp_render()
+        elif argv[0] == '-s':
+            render_map = cam.render()
+    for iter in render_map:
+        if iter[2]:
+            canv.create_rectangle(iter[0], iter[1], iter[0], iter[1], outline='white')
+    canv.update()
+    print("Rendering complete!")
+    window.mainloop()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
